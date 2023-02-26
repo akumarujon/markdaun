6b262e7a064db0d2e6d2ad6df437ab694b588558
user6879 <bpoliar01@gmail.com>
2023-02-26 14:01:00 +0500

    flag support

--- src/main.v
+++ src/main.v
@@ -1,13 +1,21 @@
 module main
 
+import flag
 import os
 import markdown
 
 fn main() {
-	text := os.args[1]
-	lines := os.read_file(text)!
+mut fp := flag.new_flag_parser(os.args)
+    fp.application('markdaun')
+    fp.version('v0.0.1')
+    fp.description('MD2HTML tool wtitten in V.')
+    fp.skip_executable()
+    input := fp.string('input', `i`, "Input is required","Input file(Markdown file)")
+	output := fp.string('output', `o`, "Output is required", "Output file(HTML file)")
+	
+	lines := os.read_file(input)!
 	md := markdown.to_html(lines)
-	os.write_file("output.html",'<!DOCTYPE html>
+	os.write_file(output,'<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
